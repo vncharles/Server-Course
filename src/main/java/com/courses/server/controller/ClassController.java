49a0e4aa6d7710e789e37fed47400d6460e35de9
vncharles <80318266+vncharles@users.noreply.github.com>
2022-10-16 02:18:03 +0700

    ok

--- src/main/java/com/courses/server/controller/ClassController.java
+++ src/main/java/com/courses/server/controller/ClassController.java
@@ -4,6 +4,9 @@ import com.courses.server.dto.MessageResponse;
 import com.courses.server.dto.request.ClassRequest;
 import com.courses.server.dto.response.ClassDTO;
 import com.courses.server.entity.Class;
+import com.courses.server.entity.ERole;
+import com.courses.server.entity.Role;
+import com.courses.server.repositories.RoleRepository;
 import com.courses.server.services.ClassService;
 import com.courses.server.utils.Authen;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +15,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/class")
@@ -26,18 +28,28 @@ public class ClassController {
     @Autowired
     private ClassService classService;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @GetMapping("")
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_TRAINER')")
-        public ResponseEntity<?> getAllClass(@Param("package") String packages){
+        public ResponseEntity<?> getAllClass(@Param("packages") String packages){
         Authen.check();
         List<Class> classList = classService.getAllClass();
+
+        Role role = null;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        for(GrantedAuthority rolee: auth.getAuthorities()){
+            role = roleRepository.findByName(ERole.valueOf(rolee.getAuthority())).get();
+        }
 
         List<ClassDTO> classDTOList = new ArrayList<>();
         if(packages!=null){
             for(Class _class: classList) {
-                if(_class.getPackages().contains(packages)) {
-                    classDTOList.add(new ClassDTO(_class));
-                }
+                if(_class.getPackages()!=null)
+                    if(_class.getPackages().contains(packages)) {
+                        classDTOList.add(new ClassDTO(_class));
+                    }
             }
         } else {
             for (Class _class : classList) {
@@ -45,7 +57,18 @@ public class ClassController {
             }
         }
 
-        return ResponseEntity.ok(classDTOList);
+        List<ClassDTO> listFinal = new ArrayList<>();
+        if(role.getName().equals(ERole.ROLE_TRAINER)) {
+            for(ClassDTO _class: classDTOList){
+                if(_class.getTrainer()!=null) {
+                    if(_class.getTrainer().getUsername().equals(auth.getName())) {
+                        listFinal.add(_class);
+                    }
+                }
+            }
+        } else  listFinal = classDTOList;
+
+        return ResponseEntity.ok(listFinal);
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_TRAINER')")
